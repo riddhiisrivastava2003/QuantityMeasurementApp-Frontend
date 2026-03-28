4a70e42892eb86edb8420bb448d25ea10223b66a
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2026-03-28 12:55:53 +0530

    [Riddhi Srivastava] Added google auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId="110405843624-sotbcfgc7qgcp7ahebjdn8uh70buj388.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="Client_id">
     <App />
   </GoogleOAuthProvider>
 );
